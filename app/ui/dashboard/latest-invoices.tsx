1fff02a4c94621c20a53ab5ca71bacf17c4f3616
David Payet <davidpayet570@gmail.com>
2023-11-22 19:49:23 +0100

    Search & Pagination

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,10 +2,10 @@ import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices } from '@/app/lib/data'
+import { fetchLatestInvoices } from '@/app/lib/data';
 
-export default async function LatestInvoices(){
-  const latestInvoices = await fetchLatestInvoices()
+export default async function LatestInvoices() {
+  const latestInvoices = await fetchLatestInvoices();
 
   return (
     <div className="flex w-full flex-col md:col-span-4 lg:col-span-4">
